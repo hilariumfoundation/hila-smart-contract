1eeef0f1b17656f10dae661e9e1fcc070dd50820
hilariumfoundation <hilariumdev@gmail.com>
2018-08-03 18:08:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hilarium ERC20 Token
+Current version:0.4.24+commit.e67f0147.Emscripten.clang
+
+address: 0xFc4E29c93eed79Df1D8254Aaa0c21463aE85Fb65  
+
+http://www.hilarium.org.sg/
---
+++ token/ManagedToken.sol
@@ -0,0 +1,173 @@
+pragma solidity ^0.4.21;
+
+import "../ownership/Ownable.sol";
+import "./ERC20Token.sol";
+import "./ITokenEventListener.sol";
+
+/**
+ * @title ManagedToken
+ * @dev ERC20 compatible token with issue and destroy facilities
+ * @dev All transfers can be monitored by token event listener
+ */
+contract ManagedToken is ERC20Token, Ownable {
+    bool public allowTransfers = false;                                         //Default not transfer
+    bool public issuanceFinished = false;                                       //Finished issuance
+
+    ITokenEventListener public eventListener;                                   //Listen events
+
+    event AllowTransfersChanged(bool _newState);                                //Event:
+    event Issue(address indexed _to, uint256 _value);                           //Event: Issue
+    event Destroy(address indexed _from, uint256 _value);                       //Event:
+    event IssuanceFinished();                                                   //Event: Finished issuance
+
+    //Modifier: Allow all transfer if not any condition
+    modifier transfersAllowed() {
+        require(allowTransfers);
+        _;
+    }
+
+    //Modifier: Allow continue to issue
+    modifier canIssue() {
+        require(!issuanceFinished);
+        _;
+    }
+
+    /**
+     * @dev ManagedToken constructor
+     * @param _listener Token listener(address can be 0x0)
+     * @param _owner Owner of contract(address can be 0x0)
+     */
+    constructor(address _listener, address _owner) public Ownable(_owner) {
+        if(_listener != address(0)) {
+            eventListener = ITokenEventListener(_listener);
+        }
+    }
+
+    /**
+     * @dev Enable/disable token transfers. Can be called only by owners
+     * @param _allowTransfers True - allow False - disable
+     */
+    function setAllowTransfers(bool _allowTransfers) external onlyOwner {
+        allowTransfers = _allowTransfers;
+
+        //Call event
+        emit AllowTransfersChanged(_allowTransfers);
+    }
+
+    /**
+     * @dev Set/remove token event listener
+     * @param _listener Listener address (Contract must implement ITokenEventListener interface)
+     */
+    function setListener(address _listener) public onlyOwner {
+        if(_listener != address(0)) {
+            eventListener = ITokenEventListener(_listener);
+        } else {
+            delete eventListener;
+        }
+    }
+
+    function transfer(address _to, uint256 _value) public transfersAllowed returns (bool) {
+        bool success = super.transfer(_to, _value);
+        /* if(hasListener() && success) {
+            eventListener.onTokenTransfer(msg.sender, _to, _value);
+        } */
+        return success;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public transfersAllowed returns (bool) {
+        bool success = super.transferFrom(_from, _to, _value);
+
+        //If has Listenser and transfer success
+        /* if(hasListener() && success) {
+            //Call event listener
+            eventListener.onTokenTransfer(_from, _to, _value);
+        } */
+        return success;
+    }
+
+    function hasListener() internal view returns(bool) {
+        if(eventListener == address(0)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @dev Issue tokens to specified wallet
+     * @param _to Wallet address
+     * @param _value Amount of tokens
+     */
+    function issue(address _to, uint256 _value) external onlyOwner canIssue {
+        require(totalSupply >= _value);
+        totalSupply = safeSub(totalSupply, _value);
+        balances[_to] = safeAdd(balances[_to], _value);
+        //Call event
+        emit Issue(_to, _value);
+        emit Transfer(address(0), _to, _value);
+    }
+
+    /**
+     * @dev Destroy tokens on specified address (Called byallowance owner or token holder)
+     * @dev Fund contract address must be in the list of owners to burn token during refund
+     * @param _from Wallet address
+     * @param _value Amount of tokens to destroy
+     */
+    function destroy(address _from, uint256 _value) external onlyOwner {
+        require(balances[_from] >= _value);
+
+        totalSupply = safeAdd(totalSupply, _value);
+        balances[_from] = safeSub(balances[_from], _value);
+
+        emit Transfer(_from, address(0), _value);
+        //Call event
+        emit Destroy(_from, _value);
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an owner allowed to a spender.
+     *
+     * approve should be called when allowed[_spender] == 0. To increment
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From OpenZeppelin StandardToken.sol
+     * @param _spender The address which will spend the funds.
+     * @param _addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = safeAdd(allowed[msg.sender][_spender], _addedValue);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an owner allowed to a spender.
+     *
+     * approve should be called when allowed[_spender] == 0. To decrement
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From OpenZeppelin StandardToken.sol
+     * @param _spender The address which will spend the funds.
+     * @param _subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = safeSub(oldValue, _subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    /**
+     * @dev Finish token issuance
+     * @return True if success
+     */
+    function finishIssuance() public onlyOwner returns (bool) {
+        issuanceFinished = true;
+        //Call event
+        emit IssuanceFinished();
+        return true;
+    }
+}
